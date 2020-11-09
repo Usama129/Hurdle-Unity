eddfe2b434bd7e803af0fb1a465ff0284d6eb789
usama <usamahumayun@gmail.com>
2020-11-09 04:19:32 +0300

    Fixed movement of Avatar
    Fixed animative movements of Avatar: Up, Left, Right. Fixed (removed) jump within jump. Jump is now a force added in Impulse Mode. Various stages of jump animation now execute at appropriate stages of the jump cycle. Cleaned Avatar.cs (previously player.cs), modularized code.

---
+++ Assets/Avatar.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Avatar : MonoBehaviour
+{
+    private Animator jumpAnim;
+    private GameObject ground;
+    private float jumpForce = 8;
+
+    void Start()
+    {
+        ground = GameObject.Find("Ground");
+        jumpAnim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) && IsGrounded())
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jumpAnim.Play("Start Jump");
+        }
+
+        if (currentStateMatches("Start Jump"))
+        {
+            if (VerticalVelocity() < 0.5 && VerticalVelocity() > 0)
+            {
+                jumpAnim.Play("Midair");
+            }
+        }
+
+        if (currentStateMatches("Midair"))
+        {
+            if (DistanceToGround() < 2.5 && GetComponent<Rigidbody>().velocity.y < 0)
+            {
+                jumpAnim.Play("End Jump");
+            }
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.right * 1, ForceMode.Impulse);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.left * 1, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("ground"))
+        {
+
+        }
+    }
+
+    private float DistanceToGround()
+    {
+        return Vector3.Distance(GetComponent<Rigidbody>().position, ground.transform.position);
+    }
+
+    private bool IsGrounded()
+    {
+        return DistanceToGround() < 0.6; // When Avatar is on the ground, DistanceToGround() returns around 0.54
+    }
+
+    private float VerticalVelocity()
+    {
+        return GetComponent<Rigidbody>().velocity.y;
+    }
+
+    private bool currentStateMatches(string state)
+    {
+        return jumpAnim.GetCurrentAnimatorStateInfo(0).IsName(state);
+    }
+}
